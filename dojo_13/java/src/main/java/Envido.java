28bf2608380df00b473be45e4569b879358f2b1d
diegosanchez <diego.sanchez@mercadolibre.com>
2018-07-06 09:39:28 -0300

    [envido] Nos independizamos del orden de las cartas

--- dojo_13/java/src/main/java/Envido.java
+++ dojo_13/java/src/main/java/Envido.java
@@ -1,24 +1,50 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Envido {
-    private Carta carta1;
-    private Carta carta2;
-    private Carta carta3;
+    private final ArrayList<Carta> cartas;
 
     public Envido(Carta carta1, Carta carta2, Carta carta3) {
-        this.carta1 = carta1;
-        this.carta2 = carta2;
-        this.carta3 = carta3;
+        this.cartas = new ArrayList<Carta>( Arrays.asList(carta1,carta2,carta3));
     }
 
     public Integer valor() {
-        Carta carta = this.carta1;
-        Integer resultado = this.carta1.numero();
+        Integer maximo = 1;
 
-        if( carta.palo().equals(this.carta2.palo()) ){
-            resultado = resultado + this.carta2.numero();
+        for(int i = 0; i < this.cartas.size(); ++i) {
+            Integer valor = this.valorTomandoCarta(i);
+
+            if ( valor >= maximo) {
+                maximo = valor;
+            }
         }
-        if( carta.palo().equals(this.carta3.palo()) ){
-            resultado = resultado + this.carta3.numero();
+
+        return maximo + 20;
+
+    }
+
+    private Integer valorTomandoCarta(int i) {
+        Carta carta = this.cartas.get(i);
+        Integer resultado = carta.numero();
+
+        ArrayList<Carta> restantes = (ArrayList<Carta>) this.cartas.clone();
+        restantes.remove(i);
+
+
+        if( carta.palo().equals(restantes.get(0).palo() ) ){
+            resultado = resultado + restantes.get(0).numero();
         }
+        if( carta.palo().equals(restantes.get(1).palo()) ){
+            resultado = resultado + restantes.get(1).numero();
+        }
+
         return resultado;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        Envido another = (Envido)other;
+
+        return this.cartas.containsAll( another.cartas );
     }
 }
--- dojo_13/java/src/test/java/EnvidoTest.java
+++ dojo_13/java/src/test/java/EnvidoTest.java
@@ -1,19 +1,45 @@
-import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class EnvidoTest {
 
     @Test
     public void envido_de_mano_uno_pierde_contra_envido_de_mano_dos(){
-        Mano manoUno = new Mano(new TresOro(), new CincoOro(), new AnchoEspada());
-        Mano manoDos = new Mano(new SieteEspada(), new CincoEspadas(), new AnchoBasto());
+        Envido envido1 = new Envido(new TresOro(), new CincoOro(), new AnchoEspada());
+        Envido envido2 = new Envido(new SieteEspada(), new CincoEspadas(), new AnchoBasto());
 
-        Integer envidoUno = manoUno.valorEnvido();
-        Integer envidoDos = manoDos.valorEnvido();
+        Integer valor1 = envido1.valor();
+        Integer valor2 = envido2.valor();
 
-        assertTrue( envidoUno < envidoDos);
+        assertTrue( valor1 < valor2);
+    }
+
+    @Test
+    public void envido_es_igual_independientemente_del_orden_de_sus_cartas(){
+        Envido envido1 = new Envido(new SieteEspada(), new CincoEspadas(), new AnchoBasto());
+        Envido envido2 = new Envido(new SieteEspada(), new AnchoBasto(), new CincoEspadas());
+
+        assertEquals( envido1, envido2);
+    }
+
+    @Test
+    public void envido_valor_debe_ser_igual_para_cualquier_orden_de_cartas(){
+        Envido envido1 = new Envido(new SieteEspada(), new CincoEspadas(), new AnchoBasto());
+        Envido envido2 = new Envido(new SieteEspada(), new AnchoBasto(), new CincoEspadas());
+        Envido envido3 = new Envido(new AnchoBasto(), new SieteEspada(), new CincoEspadas());
+        Envido envido4 = new Envido(new CincoEspadas(), new AnchoBasto(), new SieteEspada());
+
+        Integer valor1 = envido1.valor();
+        Integer valor2 = envido2.valor();
+        Integer valor3 = envido3.valor();
+        Integer valor4 = envido4.valor();
+
+        assertEquals(valor1, new Integer(32));
+        assertEquals(valor1, valor2);
+        assertEquals(valor2, valor3);
+        assertEquals(valor3, valor4);
     }
 
 
